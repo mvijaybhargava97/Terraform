5332b5a487c25ee15128286299b79c23a4c767e9
Vijay Bhargava <mvijaybhargav97@gmail.com>
2022-01-27 21:13:09 +0530

    Added the Configuration Files to create RG

---
+++ Configuration Files/Provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm"{
+    subscription_id = "848bffad-a9ae-486f-a160-76388acf2e31"
+    client_id = "f2fdc7c8-d30a-4497-bfb9-b56824f22526"
+    client_secret = "uwy7Q~Mn2hALFQwe-u8hZwr8wR6o.zc2KziKV"
+    tenant_id = "808cc83e-a546-47e7-a03f-73a1ebba24f3"
+    features {}
+}
