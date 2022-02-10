da872365749b52fad4960d44ac6801f39a3807b2
mvijaybhargava97 <98476143+mvijaybhargava97@users.noreply.github.com>
2022-02-10 22:32:14 +0530

    commit

--- Modules/Storage/Outputs.tf
+++ Modules/Storage/Outputs.tf
@@ -3,7 +3,7 @@ output "primary_access_key"{
     description = "The Primary Access Key of the Storage Account"
 }
 
-output "subnets list"{
+output "subnets_list"{
     value = data.azurerm_subnet.subnet[each.value].id
     description = "The Subnet IDs of all the subnets in the vnet"
 }
