158b8ad48d1ad6bba321054a624b8af4d5fe4677
mvijaybhargava97 <98476143+mvijaybhargava97@users.noreply.github.com>
2022-02-11 09:59:22 +0530

    Changes

--- Modules/Storage/Outputs.tf
+++ Modules/Storage/Outputs.tf
@@ -2,9 +2,3 @@ output "primary_access_key"{
     value = azurerm_storage_account.storage.primary_access_key
     description = "The Primary Access Key of the Storage Account"
 }
-
-
-output "subnets_list"{
-    value = data.azurerm_subnet.subnet[each.key].id
-    description = "The Subnet IDs of all the subnets in the vnet"
-}
