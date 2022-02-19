8b05e9cd20effaa272f9a93718fcd6701116b8d1
mvijaybhargava97 <98476143+mvijaybhargava97@users.noreply.github.com>
2022-02-19 22:43:13 +0530

    Updated Jenkins File

--- Configuration Files/CreateStorageWithNetworkRules.tf
+++ Configuration Files/CreateStorageWithNetworkRules.tf
@@ -6,6 +6,6 @@ module "Storage"{
     rg-location          =    azurerm_resource_group.app_grp.location
     storage-tier         =    "Standard"
     storage-replication  =    "LRS"
-    vnet-name            =    "vijaytfstorage"
+    vnet-name            =    module.VNet.vnet-name
     vnet-rg-name         =    azurerm_resource_group.app_grp.name
 }
