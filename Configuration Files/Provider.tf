7cf9617e416bb59c790dbf5137130b8c663ccb37
mvijaybhargava97 <98476143+mvijaybhargava97@users.noreply.github.com>
2022-02-20 12:35:36 +0530

    Updated Azure Details

--- Configuration Files/Provider.tf
+++ Configuration Files/Provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
     subscription_id = "848bffad-a9ae-486f-a160-76388acf2e31"
     client_id = "785698ee-562d-4873-8edb-1fa690d82e5a"
-    client_secret = "shl7Q~zjLklTwjY2R0GVgzZuMuY1aR-KCtZ~Q"
-    tenant_id = "MeS7Q~gSBV7JnVYv.ugujSn4ejvAYqAiP1RLW"
+    client_secret = "AMw7Q~JDyg48q-yKBRWNUTxEBxIMNjpcCsjU7"
+    tenant_id = "808cc83e-a546-47e7-a03f-73a1ebba24f3"
     features {}
 }
 /*
